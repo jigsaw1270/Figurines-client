8c9ff916dff147fba179e637ef6b0fe0d5475828
jigsaw1270 <nomadshomoy@gmail.com>
2023-05-23 21:01:31 +0600

    banner fixed

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div className="w-full carousel">
+            <div className="w-4/5 mx-auto carousel">
   <div id="item1" className="w-full carousel-item">
-    <img src="https://i.ibb.co/d4TnvXs/Website-banner-collectibles.webp" className="w-full" />
+    <img src="https://i.ibb.co/FBqySZ3/dc-superheroes-ji-1600x900.jpg" className="w-full" />
   </div> 
   <div id="item2" className="w-full carousel-item">
-    <img src="https://i.ibb.co/XW5Bq1t/00e38b76-13bf-4e0a-916d-1911d9a130d9.jpg" className="w-full" />
+    <img src="https://i.ibb.co/Y0YsR7t/batman-dark-4k-art-ip-1600x900.jpg" className="w-full" />
   </div> 
   <div id="item3" className="w-full carousel-item">
-    <img src="https://i.ibb.co/Qf59hGx/rare-wwe-action-figures-hero.webp" className="w-full" />
+    <img src="https://i.ibb.co/Wsr7BxM/portgas-d-ace-bp-1600x900.jpg" className="w-full" />
   </div> 
   <div id="item4" className="w-full carousel-item">
-    <img src="https://i.ibb.co/sqSdRst/batman-action-figures-1674667439172.jpg" className="w-full" />
+    <img src="https://i.ibb.co/7KZ6PDf/moon-knight-5k-art-5i-1600x900.jpg" className="w-full" />
   </div>
 </div> 
 <div className="flex justify-center w-full gap-2 py-2">
